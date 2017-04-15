e3ba881b896f2076f59e726dc71c0764bd676ef6
waicool20 <waicool20@gmail.com>
2017-04-15 19:33:21 +0800

    Added support for running out of jar (for IDEs)

--- src/main/kotlin/com/waicool20/sikulicef/util/CefResourceLoader.kt
+++ src/main/kotlin/com/waicool20/sikulicef/util/CefResourceLoader.kt
@@ -20,42 +20,45 @@ package com.waicool20.sikulicef.util
 import java.net.URI
 import java.nio.file.FileSystems
 import java.nio.file.Files
+import java.nio.file.Paths
 import java.nio.file.attribute.PosixFilePermission
 
 
 object CefResourceLoader {
     fun load() {
-        val jarPath = javaClass.protectionDomain.codeSource.location.toURI().path
-        val jarURI = URI.create("jar:file:$jarPath")
-        val env = mapOf(
-                "create" to "false",
-                "encoding" to "UTF-8"
-        )
-        val libDir = Files.createTempDirectory("java-cef-res")
-        if (Files.notExists(libDir)) Files.createDirectories(libDir)
+        val codeSource = javaClass.protectionDomain.codeSource.location.toURI().path
+        val libDir = if (codeSource.endsWith(".jar")) Files.createTempDirectory("java-cef-res") else Paths.get("")
+        if (codeSource.endsWith(".jar")) {
+            val jarURI = URI.create("jar:file:$codeSource")
+            val env = mapOf(
+                    "create" to "false",
+                    "encoding" to "UTF-8"
+            )
+            if (Files.notExists(libDir)) Files.createDirectories(libDir)
 
-        (FileSystems.newFileSystem(jarURI, env)).use { fs ->
-            Files.walk(fs.getPath("/java-cef-res"))
-                    .filter { it.nameCount > 1 }
-                    .map { it to libDir.resolve((if (it.nameCount > 2) it.subpath(1, it.nameCount) else it.fileName).toString()) }
-                    .forEach {
-                        Files.copy(it.first, it.second)
-                        val perms = Files.getPosixFilePermissions(it.second).toMutableSet()
-                        perms.addAll(listOf(PosixFilePermission.OWNER_EXECUTE, PosixFilePermission.GROUP_EXECUTE, PosixFilePermission.OTHERS_EXECUTE))
-                        Files.setPosixFilePermissions(it.second, perms)
-                    }
+            (FileSystems.newFileSystem(jarURI, env)).use { fs ->
+                Files.walk(fs.getPath("/java-cef-res"))
+                        .filter { it.nameCount > 1 }
+                        .map { it to libDir.resolve((if (it.nameCount > 2) it.subpath(1, it.nameCount) else it.fileName).toString()) }
+                        .forEach {
+                            Files.copy(it.first, it.second)
+                            val perms = Files.getPosixFilePermissions(it.second).toMutableSet()
+                            perms.addAll(listOf(PosixFilePermission.OWNER_EXECUTE, PosixFilePermission.GROUP_EXECUTE, PosixFilePermission.OTHERS_EXECUTE))
+                            Files.setPosixFilePermissions(it.second, perms)
+                        }
+            }
+            Runtime.getRuntime().addShutdownHook(Thread {
+                Files.walk(libDir)
+                        .sorted(Comparator.reverseOrder())
+                        .forEach(Files::delete)
+            })
         }
+
 
         Files.walk(libDir)
                 .filter { it.toString().endsWith(".so") || it.toString().endsWith(".dll") }
                 .forEach {
                     SystemUtils.loadLibrary(it)
                 }
-
-        Runtime.getRuntime().addShutdownHook(Thread {
-            Files.walk(libDir)
-                    .sorted(Comparator.reverseOrder())
-                    .forEach(Files::delete)
-        })
     }
 }
