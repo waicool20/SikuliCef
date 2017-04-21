df37d6ce850a7f89e5a2b7ee97ce278280638ad6
waicool20 <waicool20@gmail.com>
2017-04-21 16:15:07 +0800

    Leaving only awaited values in blocking thread

--- src/main/kotlin/com/waicool20/sikulicef/util/ConcurrentUtils.kt
+++ src/main/kotlin/com/waicool20/sikulicef/util/ConcurrentUtils.kt
@@ -21,13 +21,13 @@ import kotlinx.coroutines.experimental.*
 import kotlin.coroutines.experimental.CoroutineContext
 
 inline fun <T> Iterable<T>.parallelForEach(crossinline action: (T) -> Unit, pool: CoroutineContext = CommonPool): Unit {
+    val jobs = mutableListOf<Job>()
+    for (element in this@parallelForEach) {
+        launch(pool) {
+            action(element)
+        }.let(jobs::add)
+    }
     runBlocking {
-        val jobs = mutableListOf<Job>()
-        for (element in this@parallelForEach) {
-            launch(pool) {
-                action(element)
-            }.let(jobs::add)
-        }
         jobs.forEach { it.join() }
     }
 }
@@ -37,13 +37,13 @@ inline fun <T, R> Iterable<T>.parallelMap(crossinline transform: (T) -> (R), poo
 }
 
 inline fun <T, R, C : MutableCollection<in R>> Iterable<T>.parallelMapTo(destination: C, crossinline transform: (T) -> R, pool: CoroutineContext = CommonPool): C {
+    val jobs = mutableListOf<Deferred<R>>()
+    for (item in this@parallelMapTo) {
+        async(pool) {
+            transform(item)
+        }.let(jobs::add)
+    }
     runBlocking {
-        val jobs = mutableListOf<Deferred<R>>()
-        for (item in this@parallelMapTo) {
-            async(pool) {
-                transform(item)
-            }.let(jobs::add)
-        }
         jobs.mapTo(destination, { it.await() })
     }
     return destination
